bad5a16504511dc95c84894f4a41dd3675368821
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-10-13 14:09:31 -0500

    Adding methods to the HandleRequests class for parsing url, headers, options and adding main function to Request Handler module

--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,22 @@
 import json
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class HandleRequests(BaseHTTPRequestHandler):
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        try:
+            id = int(path_params[2])
+        except IndexError:
+            pass  
+        except ValueError:
+            pass  
+
+        return (resource, id)
 
     def do_GET(self):
         """Handles GET requests to the server """
@@ -20,3 +33,37 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_DELETE(self):
         """Handles DELETE requests to the server """
         pass
+
+    def _set_headers(self, status):
+        """Sets the status code, Content-Type and Access-Control-Allow-Origin
+        headers on the response
+
+        Args:
+            status (number): the status code to return to the front end
+        """
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_OPTIONS(self):
+        """Sets the options headers
+        """
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods',
+                         'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers',
+                         'X-Requested-With, Content-Type, Accept')
+        self.end_headers()
+
+def main():
+    """Starts the server on port 8088 using the HandleRequests class
+    """
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
